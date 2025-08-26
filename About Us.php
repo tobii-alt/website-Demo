75a6caa613d1e52b020d49bde669d5bb209c5062
tobii-alt <serullomaurice04@gmail.com>
2025-08-26 21:14:54 +0300

    Add files via upload

---
+++ About Us.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <div class="cont1">
+            <div class="heading5">
+                <h5>This about US</h5>
+
+            </div>
+
+            <p>This tobii fitness<br>place where you enjoy as you bouild your body and your muscle.</p>
+        </div>
+
+    </body>
+</html>
